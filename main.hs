22bd142ebca3c7a87be62e2e502d30c04e14661f
Adam Ormandy <ormandya@gmail.com>
2018-03-31 17:10:26 +0200

    Finish main

--- main.hs
+++ main.hs
@@ -1,3 +1,16 @@
+-- FLP 1st project - PLG-2-NKA - 2018
+-- Adam Ormandy
+
+import System.IO
+import System.Environment
+import Data.Char
+import Data.List
+import Data.List.Split
+import Debug.Trace
+import Data.Maybe
+import System.Exit
+
+
 type NonTerm = String
 
 type Term = String
@@ -12,7 +25,7 @@ data Grammar = Grammar { nonterms :: [NonTerm]
                        , terms :: [Term]
                        , rules :: [Rule]
                        , beg :: NonTerm
-                       } deriving (Show)
+                       } deriving (Show)  
 
 simplifyRule :: [NonTerm] -> Rule -> [Rule]
 simplifyRule nonterms (Rule left right) = 
@@ -44,7 +57,7 @@ simplifyRule nonterms (Rule left right) =
 simplifyRules :: [NonTerm] -> [Rule] -> [Rule]
 simplifyRules nonterms rules = let splitted = splitRules nonterms rules
                                    simplified = innerSimplifyRules nonterms (fst splitted) 
-                               in simplified
+                               in simplified ++ removeEpsilonRules nonterms (snd splitted) simplified
                                -- TODO epsilon rules
 
 
@@ -54,7 +67,40 @@ innerSimplifyRules nonterms [] = []
 
 
 -- TODO rename
-findEpsilonRoute :: Nonterm -> [Rule] -> [Nonterm]
+-- prva lajna najde vsetky nove neterminali cez ktore sa vieme dostat cez epsilon
+-- TODO refactor this bitch
+-- TODO what if A ->* A
+-- TODO test this one
+-- Returns all the nonterminal reachable by 
+findEpsilonSomething :: NonTerm -> [NonTerm] -> [Rule] -> [NonTerm]
+findEpsilonSomething nonterm found_epsilon epsilon_rules = let epsilon_nonterms = [last (right r) |
+                                                                                   r <- epsilon_rules,
+                                                                                   (left r == nonterm) && (not (last (right r) `elem` found_epsilon))]
+                                                               new_found_epsilon = found_epsilon ++ epsilon_nonterms
+                                                               nonterms = concat [findEpsilonSomething new_nonterm new_found_epsilon epsilon_rules | 
+                                                                                  new_nonterm <- epsilon_nonterms]
+                                                               -- Deduplication because I am so random
+                                                               -- TODO special function
+                                                               deduplicate (x : xs) = if x `elem` xs
+                                                                                      then deduplicate xs
+                                                                                      else [x] ++ deduplicate xs
+                                                               deduplicate [] = []
+                                                           in deduplicate (nonterms ++ new_found_epsilon)
+
+
+-- TODO what if epsilon rule is there, some error shit
+-- Removes epsilon rule
+removeEpsilonRule :: NonTerm -> [NonTerm] -> [Rule] -> [Rule]
+removeEpsilonRule left_side reach_epsilon normal_rules = [(Rule left_side (right r)) |
+                                                          r <- normal_rules,
+                                                          (left r) `elem` reach_epsilon]
+
+
+-- TODO
+removeEpsilonRules :: [NonTerm] -> [Rule] -> [Rule] -> [Rule]
+removeEpsilonRules (nonterm:rest) epsilon_rules normal_rules = removeEpsilonRule nonterm (findEpsilonSomething nonterm [] epsilon_rules) normal_rules
+                                                               ++ removeEpsilonRules rest epsilon_rules normal_rules
+removeEpsilonRules [] epsilon_rules normal_rules = []
 
 
 -- ([Rule], [Rule])
@@ -72,7 +118,7 @@ splitRules nonterms [] = ([], [])
 getNonTerminals :: [Rule] -> [Term] -> [NonTerm]
 getNonTerminals rules terms = deduplicate (parse rules terms)
                               where
-                                parse (rule : rules) terms = [left rule] ++ [nonterm | nonterm <- right rule, not (nonterm `elem` terms)] ++ parse rules terms
+                                parse (rule : rules) terms = [left rule] ++ [nonterm | nonterm <- right rule, not (nonterm `elem` (["#"] ++ terms))] ++ parse rules terms
                                 parse [] terms = []
                                 deduplicate (x : xs) = if x `elem` xs
                                                        then deduplicate xs
@@ -116,6 +162,10 @@ convertGrammarToMachine (Grammar nonterms terms rules beg) = let mapping = creat
                                                              in Machine states terms start_state transitions end_states
 
 
+-- TODO find end states
+-- TODO remove A -> # -> END
+
+
 -- TODO check
 -- TODO some function
 ruleToTransition :: Rule -> [(NonTerm, State)] -> Transition
@@ -150,3 +200,68 @@ createState nonterm states =
             then customState nonterm states (1 + suffix)
             else nonterm ++ show suffix
 
+
+-- TODO check input
+-- TODO main
+-- TODO test everything
+
+-- #MAIN
+main :: IO ()
+main = do
+  -- read arguments and parse arguments
+  args <- getArgs 
+  let (option, inputSource) = parseArgs args
+  -- read input
+  input <- fmap lines $ if (inputSource==[]) 
+      then getContents 
+      else readFile inputSource
+  -- parse input into Grammar
+  let grammar = parseInputGrammar input
+  case option of 0 -> printGrammar grammar
+                 1 -> printGrammar $ simplifyGrammar grammar
+                 2 -> printMachine $ convertGrammarToMachine $ simplifyGrammar grammar
+  return ()
+
+
+parseArgs :: [String] -> (Integer, String)
+parseArgs [] = (2, [])
+parseArgs [x]
+  | x=="-i" = (0, [])
+  | x=="-1" = (1, [])
+  | x=="-2" = (2, [])
+  | otherwise = (2, [])
+parseArgs [x,y]
+  | x=="-i" = (0, y)
+  | x=="-1" = (1, y)
+  | x=="-2" = (2, y)
+  | otherwise = (2, y)
+parseArgs _ = error "Something is wrong with the arguments."
+
+
+parseInputGrammar :: [String] -> Grammar
+parseInputGrammar (nonterms : terms : beg_nonterm : rules) =
+  Grammar (splitOn "," nonterms)
+          (splitOn "," terms)
+          -- Parse rules
+          [Rule (head dummy) [[x] | x <- (last dummy)]| dummy  <- [splitOn "->" r | r <- rules]]
+          beg_nonterm
+
+
+printGrammar :: Grammar -> IO ()
+printGrammar grammar = do putStrLn (intercalate "," (nonterms grammar))
+                          putStrLn (intercalate "," (terms grammar))
+                          putStrLn (beg grammar)
+                          mapM_ putStrLn [(left r) ++ "->" ++ (intercalate "" (right r)) | r <- rules grammar]
+
+
+printMachine :: Machine -> IO ()
+printMachine (Machine states _ start_state transitions end_states) =
+  do putStrLn (intercalate "," [show s | s <- states])
+     putStrLn $ show start_state
+     putStrLn (intercalate "," [show s | s <- end_states])
+     mapM_ putStrLn [(show $ origin t)
+                     ++ ","
+                     ++ (show $ symbol t)
+                     ++ ","
+                     ++ (show $ dest t) 
+                     | t <- transitions]
